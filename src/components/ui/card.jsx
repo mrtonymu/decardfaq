967db1b3d1cc8a8ba6e9f7dc77231c33c88bf4be
Mr.Wood <90689134+mrtonymu@users.noreply.github.com>
2025-05-14 13:21:55 +0800

    Delete src directory

--- src/components/ui/card.jsx
+++
@@ -1,2 +0,0 @@
-export const Card = ({ children }) => <div className="border rounded shadow-sm">{children}</div>;
-export const CardContent = ({ children, className }) => <div className={className}>{children}</div>;
