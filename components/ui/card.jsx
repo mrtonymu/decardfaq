3309dfd87e7f08725e021af8c895282e0b5eae8b
Mr.Wood <90689134+mrtonymu@users.noreply.github.com>
2025-05-14 13:24:28 +0800

    Add files via upload

---
+++ components/ui/card.jsx
@@ -0,0 +1,2 @@
+export const Card = ({ children }) => <div className='border rounded shadow-sm'>{children}</div>;
+export const CardContent = ({ children, className }) => <div className={className}>{children}</div>;
