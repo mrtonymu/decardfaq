71ff6f8ac78066cc4975c667ddfde1d42e1402ed
Mr.Wood <90689134+mrtonymu@users.noreply.github.com>
2025-05-14 14:15:31 +0800

    Add React import to card component

--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -1,2 +1,4 @@
+import React from 'react';
+
 export const Card = ({ children }) => <div className='border rounded shadow-sm'>{children}</div>;
 export const CardContent = ({ children, className }) => <div className={className}>{children}</div>;
